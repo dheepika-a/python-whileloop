2473a548d683a7ff5ac584046b9321b76aec844f
unknown <dheepi117@gmail.com>
2022-12-26 12:35:48 +0530

    adding file.

---
+++ whileloop.py
@@ -0,0 +1,20 @@
+i =2
+while i < 5:
+    print(i)
+    i +=1
+
+#array
+m = ["dheep","age","sibling"]
+
+while m:
+    print(m.pop())
+
+#count
+a = 'i am dheepika'
+i = 0
+  
+while i < len(a):
+    i += 1
+    pass
+    
+print(i)
